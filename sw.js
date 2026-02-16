bec1dc6ed0e00bb2cc90055b6ec228114d84eac1
Hussien Mohammed <9mh9a@gmail.com>
2026-02-16 18:20:47 +0300

    Converted project to HTML CSS JS version

--- sw.js
+++ sw.js
@@ -1,21 +1,23 @@
-
-const CACHE_NAME = 'duty-manager-v2';
+const CACHE_NAME = 'shift-app-v2';
 const urlsToCache = [
   './',
   './index.html',
+  './style.css',
+  './script.js',
   './manifest.json',
-  'https://cdn.tailwindcss.com',
-  'https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;600;700;900&display=swap'
+  'https://fonts.googleapis.com/css2?family=Tajawal:wght@400;500;700;900&display=swap'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
